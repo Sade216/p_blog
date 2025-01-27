b162f7d52c324bc8ee7f7017bf1f9a55ae5406a0
sade216 <vlad.tok.1997@mail.ru>
2025-01-27 23:38:38 +0700

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useEffect, useState } from 'react'
+// import { type ApiRoutes } from '../../server/app'
+// import { hc } from 'hono/client'
+
+// const client = hc<ApiRoutes>('/')
+ 
+
+function App() {
+ 
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
